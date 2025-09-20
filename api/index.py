57ecd620e6c56aef010a222931e9e7d75457cafc
SRKChaithanya <128833020+SRKChaithanya@users.noreply.github.com>
2025-09-20 12:35:29 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template
 import os
 
-app = Flask(__name__)
+app = Flask(__name__,
+           template_folder='../templates',
+           static_folder='../static')
 app.secret_key = os.environ.get('SESSION_SECRET', 'dev-secret-key')
 
 @app.route('/')
